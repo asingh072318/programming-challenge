2a0d625287c408ac92801978ea2ea89bf7b9f91a
asingh072318 <ankit.bitmsra@gmail.com>
2022-08-27 17:43:38 -0400

    run on host port 9999 for development

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 if [[ "$(docker images -q move_tracker 2> /dev/null)" == "" ]]; then
   /bin/bash build_image.sh
 fi
-docker run --rm --name mtracker_container -e POSTGRES_PASSWORD=testpass -it -v $HOME:/root -d move_tracker 2> /dev/null
+docker kill mtracker_container
+docker run --rm --name mtracker_container -e POSTGRES_PASSWORD=testpass -it -v $HOME:/root -p 9999:5432 -d move_tracker 2> /dev/null
 docker exec -it mtracker_container /bin/bash
