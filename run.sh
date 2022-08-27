81cbad1c79f180955935a9a48eb69bd7ca152a4b
asingh072318 <ankit.bitmsra@gmail.com>
2022-08-27 17:18:00 -0400

    Removed unnecessary message

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 if [[ "$(docker images -q move_tracker 2> /dev/null)" == "" ]]; then
   /bin/bash build_image.sh
 fi
-docker run --rm --name mtracker_container -e POSTGRES_PASSWORD=testpass -it -v $HOME:/root -d move_tracker
+docker run --rm --name mtracker_container -e POSTGRES_PASSWORD=testpass -it -v $HOME:/root -d move_tracker 2> /dev/null
 docker exec -it mtracker_container /bin/bash
