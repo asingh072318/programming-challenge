bb97342d11bd5501ab7eb22dfc67d91af1a83cc1
asingh072318 <ankit.bitmsra@gmail.com>
2022-08-29 14:33:07 -0400

    Modified Run script

--- run.sh
+++ run.sh
@@ -6,5 +6,7 @@ docker kill mtracker_container
 docker run --rm --name mtracker_container -e POSTGRES_PASSWORD=testpass -it -v $HOME:/root -p 9999:5432 -d move_tracker 2> /dev/null
 echo "Waiting 5 seconds for container setup"
 sleep 5
+echo "Starting Data import on Postgres"
 docker exec -d mtracker_container python3 /home/mtracker/import_data.py
+echo "Data import completed, entering shell."
 docker exec -it mtracker_container /bin/bash
