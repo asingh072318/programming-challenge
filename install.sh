9b83be9701e3d1dcdc074677598701413883d461
asingh072318 <ankit.bitmsra@gmail.com>
2022-08-29 22:33:44 -0400

    Modified layers of Dockerfile

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
+# Check if mtracker_files directory exists and delete it
 [ -d "/usr/local/bin/mtracker_files" ] && rm -rf /usr/local/bin/mtracker_files
+# Create mtracker_files directory
 mkdir -m 777 /usr/local/bin/mtracker_files
+# Copy source code to mtracker_files
 cp -R $(pwd)/. /usr/local/bin/mtracker_files
+# creates mtracker executable in /usr/local/bin
 echo "
 #!/bin/sh
 cd /usr/local/bin/mtracker_files
 if [ "\$#" -eq 0 ]; then
     ./run.sh
-    
+
     exit 1
 fi
 case "\$1" in
