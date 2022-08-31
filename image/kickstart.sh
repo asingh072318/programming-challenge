77ecac15aae17e5e140fd7d4ff7d7568f04a5a98
asingh072318 <ankit.bitmsra@gmail.com>
2022-08-31 09:43:19 -0400

    Updated tasks for daemon run

--- image/kickstart.sh
+++ image/kickstart.sh
@@ -3,15 +3,18 @@
 echo "Starting Data import on Postgres"
 python3 /home/mtracker/import_data.py
 
-echo "Starting the MTracker API"
-cd /home/mtracker/api && supervisord
+# echo "Starting the MTracker API"
+# cd /home/mtracker/api && supervisord
 
-echo "Installing serve"
-npm install -g serve
+echo "Installing forever, serve"
+npm install -g forever serve
 
-echo "Start the MTracker Web App"
-cd /home/mtracker/spa && npm install && npm run build && supervisord
+echo "Building the MTracker Web App"
+cd /home/mtracker/spa && npm run build
+
+echo "Starting the API and Web App"
+supervisord -c /home/mtracker/supervisord.conf
 
 echo "Waiting 10 seconds for Services to start"
-
+sleep 10
 /bin/bash
--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@ if [[ "$(docker images -q move_tracker 2> /dev/null)" == "" ]]; then
   /bin/bash build_image.sh
 fi
 docker kill mtracker_container
-docker run --rm --name mtracker_container -e POSTGRES_PASSWORD=testpass -it -v $HOME:/root -p 3000:3000 -d move_tracker 2> /dev/null
+sleep 2
+docker run --rm --name mtracker_container -e POSTGRES_PASSWORD=testpass -it -v $HOME:/root -p 3000:3000 -p 8000:8000 -d move_tracker 2> /dev/null
 echo "Waiting 5 seconds for container setup"
 sleep 5
 # echo "Starting Data import on Postgres"
